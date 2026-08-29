f1d1db41aa0c7f72b62e53b89d99f354ca48c987
CJ514878 <chukwumerije.785@jesuitmemorial.org>
2026-08-29 23:51:02 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,15 +1,15 @@
 /* =========================================
-   STUDYMIND AI — SUPABASE CONFIGURATION
+STUDYMIND AI — SUPABASE CONFIGURATION
 ========================================= */
 
 const SUPABASE_URL =
-    "https://bicnrbqqvucgpbwudmit.supabase.co";
+"https://bicnrbqqvucgpbwudmit.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_70y0MPrj30-FimUSQK_HuA_Ng1a1qcB";
+"sb_publishable_70y0MPrj30-FimUSQK_HuA_Ng1a1qcB";
 
 const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_PUBLISHABLE_KEY
-    );
+window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_PUBLISHABLE_KEY
+);
